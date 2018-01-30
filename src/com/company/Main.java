fb969b7249c8b6a1f356318b9cbaf23d351677b8
Zigmas <zambrazaitis@gmail.com>
2018-01-30 15:43:58 +0200

    uzduotis8 finished

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,20 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	    // 1.8.​ Realizuoti programą, kuri apskaičiuotų kiek automobilis vidutiniškai sunaudoja kuro 100
+        // km. Programa turi paprašyti vartotojo įvesti nuvažiuotus km ir kuro sąnaudas.
+        // vidurkis = (kuro sąnaudos *100)/ nuvažiuoto atstumo
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Iveskite nuvaziuotus km: ");
+        int km = sc.nextInt();
+        System.out.println("Iveskite kuro sanaudas: ");
+        int sanaudos = sc.nextInt();
+        float vidurkis = (sanaudos*100)/km;
+        System.out.println("Kuro sanaudu vidurkis: " + vidurkis + "l.");
     }
 }
